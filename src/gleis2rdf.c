42db9d117af83fe8054690e7c491b5d846eb9b53
Sebastian Freundt <freundt@ga-group.nl>
2018-02-14 05:46:57 +0000

    ego, update copyright years

--- src/gleis2rdf.c
+++ src/gleis2rdf.c
@@ -1,6 +1,6 @@
 /*** gleis2rdf.c -- c-lei file parser
  *
- * Copyright (C) 2014-2015  Sebastian Freundt
+ * Copyright (C) 2014-2018  Sebastian Freundt
  *
  * Author:  Sebastian Freundt <freundt@ga-group.nl>
  *
